ffbe8568fd76a6cefc286ef40d5dae81cc20d95e
Grégory D'Angelo <dangelo.gregory@gmail.com>
2020-01-26 19:39:51 +0100

    feat: add cards to dashboard

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -2,17 +2,52 @@ import React from 'react';
 import styled from 'styled-components';
 // Components
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Card from '../components/Card';
 
 const ContainerStyled = styled(Container)`
-  width: 100%;
-  margin: 15px;
-  padding: 15px;
+  padding: 30px 15px;
 `;
+
+const RowStyled = styled(Row)`
+  margin-left: 0;
+  margin-right: 0;
+  justify-content: space-between;
+`;
+
+const cards = [
+  {
+    title: "63",
+    subtitle: "active referrals"
+  },
+  {
+    title: "$2,455",
+    subtitle: "estimated next payment"
+  },
+  {
+    title: "$14,570",
+    subtitle: "total payment to date"
+  },
+  {
+    title: "5,130",
+    subtitle: "referred page views"
+  }
+];
 
 const Dashboard = () => {
   return (
-    <ContainerStyled> 
-      Dashboard
+    <ContainerStyled>
+      <RowStyled>
+        {
+          cards.map((card, i) => (
+            <Card 
+              key={i}
+              title={card.title}
+              subtitle={card.subtitle}
+            />
+          ))
+        }
+      </RowStyled>
     </ContainerStyled>
   )
 };
